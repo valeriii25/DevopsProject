d865a7cab2410ac1b869caf746b1414d236532ca
Valeria Lapshina <lapshina_valeria@mail.ru>
2024-06-13 02:03:32 +0300

    Created tests for mini calculator

---
+++ DevopsProject.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿namespace DevopsProject.Tests;
+
+public class CalculatorTests
+{
+    private readonly IMiniCalculator _sut = new MiniCalculator();
+    
+    [Fact]
+    public void Should_ReturnFive_WhenTwoPlusThree()
+    {
+        Assert.Equal(5, _sut.Summarize(2 , 3));
+    }
+    
+    [Fact]
+    public void Should_ReturnThree_WhenTOnePlusTwo()
+    {
+        Assert.Equal(-3, _sut.Summarize(-1 , -2));
+    }
+    
+    [Fact]
+    public void Should_ReturnTen_WhenTFivePlusFive()
+    {
+        Assert.Equal(10, _sut.Summarize(5 , 5));
+    }
+    
+    [Fact]
+    public void Should_ReturnZero_WhenMinusOnePlusOne()
+    {
+        Assert.Equal(0, _sut.Summarize(-1 , 1));
+    }
+    
+    [Fact]
+    public void Should_ReturnOneHundred_WhenFiftyPlusFifty()
+    {
+        Assert.Equal(100, _sut.Summarize(50 , 50));
+    }
+}
